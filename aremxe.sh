4a5bf9b63009545881ef030c348a7afba4363080
midiam1 <admhimmeros@gmail.com>
2023-01-22 16:56:13 -0400

    Update aremxe.sh

--- aremxe.sh
+++ aremxe.sh
@@ -6,4 +6,20 @@ sudo apt -y update
 
 # Instalo las dependencias que me permitirán compilar MXE
 
-sudo apt-get install p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils
+sudo apt-get -y install p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils
+sudo apt-get -y install g++-multilib libc6-dev-i386
+
+# Clone mxe desde el repositorio, al descargar crea el directorio
+# desde donde vamos a trabajar
+
+cd /mnt
+git clone https://github.com/mxe/mxe.git
+
+# Compilamos boost y Qt5
+
+cd /mnt/mxe
+make MXE_TARGETS="i686-w64-mingw32.static" boost
+make MXE_TARGETS="i686-w64-mingw32.static" qttools
+
+
+
