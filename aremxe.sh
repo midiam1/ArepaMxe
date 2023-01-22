fa9d1baf8a34487b6b3fa5da75e025962cb23c47
midiam1 <admhimmeros@gmail.com>
2023-01-22 16:43:28 -0400

    Update aremxe.sh

--- aremxe.sh
+++ aremxe.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Actualizo todo todito
+
+sudo apt -y update
+
+# Instalo las dependencias que me permitirán compilar MXE
+
+sudo apt-get install p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils
