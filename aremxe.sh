880dd1e59cf27bf2a1f95c843ea4d0b808dfd71d
midiam1 <admhimmeros@gmail.com>
2023-01-22 22:48:57 -0400

    Update aremxe.sh
    Se desactivó sudo y se activó su para hacerlo todo desde root .

--- aremxe.sh
+++ aremxe.sh
@@ -5,24 +5,31 @@
 sudo apt -y update
 
 # Creamos una memoria de intercambio [ Swap File ] para no
-# tener errores al compilar
+# tener errores al compilar .
 
-sudo dd if =/dev/zero of=/swapfile bs=1024 count=1048576
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo  swapon /swapfile 
-sudo echo /swapfile swap swap defaults 0 0 >> /etc/fstab
+clear
+echo Activamos su para no estar metiendo la clave a cada rato
+
+sleep 5s
+
+sudo su
+
+dd if =/dev/zero of=/swapfile bs=1024 count=1048576
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile 
+echo /swapfile swap swap defaults 0 0 >> /etc/fstab
 
 # Instalo las dependencias que me permitirán compilar MXE
 
-sudo apt-get -y install p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils
-sudo apt-get -y install g++-multilib libc6-dev-i386
+apt-get -y install p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils
+apt-get -y install g++-multilib libc6-dev-i386
 
 # Clone mxe desde el repositorio, al descargar crea el directorio
 # desde donde vamos a trabajar
 
 cd /mnt
-sudo git clone https://github.com/mxe/mxe.git
+git clone https://github.com/mxe/mxe.git
 
 # Compilamos boost y Qt5
 
